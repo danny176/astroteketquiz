2084bbdb94393adee3d749bf2e49f37806b7d9c5
danny176 <160113158+danny176@users.noreply.github.com>
2024-11-07 10:28:57 +0100

    Kommenteret JS

--- js/video.js
+++ js/video.js
@@ -1,5 +1,4 @@
+// Indstil en timeout som skifter om til spørgsmåls/quiz siden efter 6 sekunder. Det passer med 6 sekunder da vidoen varer 6 sekunder.
 setTimeout(() => {
-  console.log("Redirecting to quizside.html after 6 seconds...");
-
-  window.location.href = "quiz.html"; // Adjust this path as necessary
-}, 6000); // 6000 milliseconds = 6 seconds
+  window.location.href = "quiz.html";
+}, 6000);
