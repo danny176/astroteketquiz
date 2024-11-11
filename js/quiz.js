92142d237b48d4edb550432e40b2327d1a13868a
danny176 <danielfrederiksmose96@gmail.com>
2024-11-11 16:39:27 +0100

    Brugertest feedback. Fjernet mulighed for at gå frem og tilbage i spørgsmål. Raket bevæger sig kun frem af ved rigtigt svar

--- js/quiz.js
+++ js/quiz.js
@@ -17,66 +17,92 @@ let userAnswers = []; // Array til at gemme brugerens svar (hvilket svar der ble
 
 // Array med spørgsmål og svarmuligheder 0, 1, 2, 3 indexer
 const quizData = [
-    {
-        question: "Hvordan blev solen dannet?",
-        options: ["I en stjernetåge", "Ved en eksplosion", "Ved kollision", "Det er uvist"],
-        correct: 0
-    },
-    {
-        question: "Hvilken farve er de varmeste stjerner?",
-        options: ["Grønne", "Blå", "Røde", "Gule"],
-        correct: 1
-    },
-    {
-        question: "Hvad er en supernova?",
-        options: ["En måde stjernerne bevæger sig på", "Når en planet bliver opslugt af et sort hul", "En overmoden stjerne, der eksploderer", "Når en komet rammer en stjerne"],
-        correct: 2
-    },
-    {
-        question: "Hvordan kan et sort hul opstå?",
-        options: ["Det er uvist", "Når en planet eksploderer", "Hvis to måner kolliderer", "Ved en supernovaeksplosion"],
-        correct: 3
-    },
-    {
-        question: "Hvad gør et sort hul?",
-        options: ["Det opsluger alt i nærheden", "Udsender lys igennem rummet", "Omdanner materialer til nye stjerner", "Hjælper galakser med at undgå at smelte sammen"],
-        correct: 0
-    },
-    {
-        question: "Hvad er en exoplanet?",
-        options: ["En planet, der konstant skifter størrelse og farve", "En planet, der er ældre end solen", "En planet, der kredser om andre stjerner end solen", "En planet, der er halvt stjerne halvt komet"],
-        correct: 2
-    },
-    {
-        question: "På hvilken planet er en liter mælk tungest?",
-        options: ["Mars", "Jupiter", "Jorden", "Uranus"],
-        correct: 1
-    },
-    {
-        question: "Hvor lang tid tager det månen at kredse rundt om jorden?",
-        options: ["30,1 dage", "60,5 dage", "27,3 dage", "12,7 dage"],
-        correct: 2
-    },
-    {
-        question: "Hvor mange jorddøgn er der på et døgn på Venus?",
-        options: ["321 jorddøgn", "75 jorddøgn", "5 jorddøgn", "243 jorddøgn"],
-        correct: 3
-    },
-    {
-        question: "Hvad er en meteorit?",
-        options: ["En partikel fra et sort hul", "Rester fra solsystemets dannelse", "Rester fra en satellit", "En samling af stjerner"],
-        correct: 1
-    }
+  {
+    question: "Hvordan blev solen dannet?",
+    options: [
+      "I en stjernetåge",
+      "Ved en eksplosion",
+      "Ved kollision",
+      "Det er uvist",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Hvilken farve er de varmeste stjerner?",
+    options: ["Grønne", "Blå", "Røde", "Gule"],
+    correct: 1,
+  },
+  {
+    question: "Hvad er en supernova?",
+    options: [
+      "En måde stjernerne bevæger sig på",
+      "Når en planet bliver opslugt af et sort hul",
+      "En overmoden stjerne, der eksploderer",
+      "Når en komet rammer en stjerne",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Hvordan kan et sort hul opstå?",
+    options: [
+      "Det er uvist",
+      "Når en planet eksploderer",
+      "Hvis to måner kolliderer",
+      "Ved en supernovaeksplosion",
+    ],
+    correct: 3,
+  },
+  {
+    question: "Hvad gør et sort hul?",
+    options: [
+      "Det opsluger alt i nærheden",
+      "Udsender lys igennem rummet",
+      "Omdanner materialer til nye stjerner",
+      "Hjælper galakser med at undgå at smelte sammen",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Hvad er en exoplanet?",
+    options: [
+      "En planet, der konstant skifter størrelse og farve",
+      "En planet, der er ældre end solen",
+      "En planet, der kredser om andre stjerner end solen",
+      "En planet, der er halvt stjerne halvt komet",
+    ],
+    correct: 2,
+  },
+  {
+    question: "På hvilken planet er en liter mælk tungest?",
+    options: ["Mars", "Jupiter", "Jorden", "Uranus"],
+    correct: 1,
+  },
+  {
+    question: "Hvor lang tid tager det månen at kredse rundt om jorden?",
+    options: ["30,1 dage", "60,5 dage", "27,3 dage", "12,7 dage"],
+    correct: 2,
+  },
+  {
+    question: "Hvor mange jorddøgn er der på et døgn på Venus?",
+    options: ["321 jorddøgn", "75 jorddøgn", "5 jorddøgn", "243 jorddøgn"],
+    correct: 3,
+  },
+  {
+    question: "Hvad er en meteorit?",
+    options: [
+      "En partikel fra et sort hul",
+      "Rester fra solsystemets dannelse",
+      "Rester fra en satellit",
+      "En samling af stjerner",
+    ],
+    correct: 1,
+  },
 ];
 
 // Tilføjer event listeners til knapperne
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => checkAnswer(index));
 });
-
-// til pilene
-pilmodvenstre.addEventListener("click", goBack);
-pilmodhøjre.addEventListener("click", goForward);
 
 function checkAnswer(selectedIndex) {
   // Tjekker om det valgte svar er korrekt
@@ -101,26 +127,12 @@ function checkAnswer(selectedIndex) {
   }, 700); // 700 millisekunder = 0,7 sekund
 }
 
-//pil tilbage
-function goBack() {
-  if (nuvaerendeStep > 0) {
-    nuvaerendeStep--;
-    loadQuestion();
-  }
-}
-
-//pil frem
-function goForward() {
-  if (nuvaerendeStep < quizData.length - 1) {
-    nuvaerendeStep++;
-    loadQuestion();
-  }
-}
-
 //Indsætter lyde til korrekt og forkert svar
-const korrektSound = new Audio("lyde/rigtig.mp3"); 
-const forkertSound = new Audio("lyde/forkert.mp3"); 
+const korrektSound = new Audio("lyde/rigtig.mp3");
+const forkertSound = new Audio("lyde/forkert.mp3");
 
+// Definer rakettens start position, så vi kan tilføje 67px ved hvert rigtig svar
+let raketPosition = 248;
 
 function updateButtonColors() {
   // Sætter knappernes farve baseret på tidligere valg - så den forbliver når man går frem og tilbage mellem spørgsmålene
@@ -129,6 +141,10 @@ function updateButtonColors() {
       if (index === quizData[nuvaerendeStep].correct) {
         button.style.backgroundColor = "green"; // Korrekt svar
         korrektSound.play(); //Lyd afspilles ved korrekt svar
+        
+        // Flyt raketten 67px når du svarer rigtig på spørgsmålet og bring det tættere 
+        raketPosition += 67;
+        document.getElementById("raketderflyver").style.left = `${raketPosition}px`;
       } else {
         button.style.backgroundColor = "red"; // Forkert svar
         forkertSound.play();
@@ -150,9 +166,6 @@ function loadQuestion() {
 
   // Opdaterer knappernes farver (i tilfælde af navigering tilbage med pilene)
   updateButtonColors();
-
-    // Flytter raketten frem når man går et sprøgsmål frem starter ved 248 px og rykker 67 px pr gang
-    document.getElementById("raketderflyver").style.left = `${248 + (67 * nuvaerendeStep)}px`;
 }
 
 // Tjekker og sender til vinder side, hvis score = 10/10 og loser siden, hvis score er 9/10 eller mindre
@@ -215,21 +228,21 @@ const sprGem = document.querySelector("#spr");
 const fremTilbage = document.querySelector("#pilknapperne");
 
 slideKnap.addEventListener("click", function () {
+  slidingImage.classList.add("slide-in");
   slideKnap.style.display = "none";
   lukKnap.style.display = "block";
   planeterne.style.opacity = "0";
   sprGem.style.opacity = "0";
   fremTilbage.style.opacity = "0";
-  slidingImage.classList.add("slide-in");
 });
 
 //Slide ud infografik når lukknap bliver trykket. Derudover gem lukknap og vis i stedet slideknap
 
 lukKnap.addEventListener("click", function () {
+  slidingImage.classList.remove("slide-in");
   lukKnap.style.display = "none";
   slideKnap.style.display = "block";
   planeterne.style.opacity = "1";
   sprGem.style.opacity = "1";
   fremTilbage.style.opacity = "1";
-  slidingImage.classList.remove("slide-in");
 });
