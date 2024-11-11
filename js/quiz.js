f6d43d2c33e73f870c7ce60fdf44659dde0da561
danny176 <danielfrederiksmose96@gmail.com>
2024-11-11 10:48:04 +0100

    Fix til quiz infografik og små ændringer i spacing og tekst

--- js/quiz.js
+++ js/quiz.js
@@ -48,7 +48,7 @@ const quizData = [
         correct: 2
     },
     {
-        question: "På hvilken planet er én l. mælk tungest?",
+        question: "På hvilken planet er en liter mælk tungest?",
         options: ["Mars", "Jupiter", "Jorden", "Uranus"],
         correct: 1
     },
@@ -71,7 +71,7 @@ const quizData = [
 
 // Tilføjer event listeners til knapperne
 buttons.forEach((button, index) => {
-    button.addEventListener("click", () => checkAnswer(index));
+  button.addEventListener("click", () => checkAnswer(index));
 });
 
 // til pilene
@@ -79,146 +79,150 @@ pilmodvenstre.addEventListener("click", goBack);
 pilmodhøjre.addEventListener("click", goForward);
 
 function checkAnswer(selectedIndex) {
-    // Tjekker om det valgte svar er korrekt
-    if (selectedIndex === quizData[nuvaerendeStep].correct) {
-        score++;
+  // Tjekker om det valgte svar er korrekt
+  if (selectedIndex === quizData[nuvaerendeStep].correct) {
+    score++;
+  }
+
+  // Gemmer brugerens svar
+  userAnswers[nuvaerendeStep] = selectedIndex;
+
+  // Farvesætter svarene (grøn for korrekt, rød for forkert)
+  updateButtonColors();
+
+  // Gå videre til næste spørgsmål efter en kort forsinkelse (1 sekund) - brugeren kan se om de har svaret rigtigt eller forkert
+  setTimeout(() => {
+    nuvaerendeStep++;
+    if (nuvaerendeStep < quizData.length) {
+      loadQuestion();
+    } else {
+      checkAndDisplayResults();
     }
-
-    // Gemmer brugerens svar
-    userAnswers[nuvaerendeStep] = selectedIndex;
-
-    // Farvesætter svarene (grøn for korrekt, rød for forkert)
-    updateButtonColors();
-
-    // Gå videre til næste spørgsmål efter en kort forsinkelse (1 sekund) - brugeren kan se om de har svaret rigtigt eller forkert
-    setTimeout(() => {
-        nuvaerendeStep++;
-        if (nuvaerendeStep < quizData.length) {
-            loadQuestion();
-        } else {
-            checkAndDisplayResults();
-        }
-    }, 700); // 700 millisekunder = 0,7 sekund
+  }, 700); // 700 millisekunder = 0,7 sekund
 }
 
 //pil tilbage
 function goBack() {
-    if (nuvaerendeStep > 0) {
-        nuvaerendeStep--;
-        loadQuestion();
-    }
+  if (nuvaerendeStep > 0) {
+    nuvaerendeStep--;
+    loadQuestion();
+  }
 }
 
 //pil frem
 function goForward() {
-    if (nuvaerendeStep < quizData.length - 1) {
-        nuvaerendeStep++;
-        loadQuestion();
-    }
+  if (nuvaerendeStep < quizData.length - 1) {
+    nuvaerendeStep++;
+    loadQuestion();
+  }
 }
 
 function updateButtonColors() {
-    // Sætter knappernes farve baseret på tidligere valg - så den forbliver når man går frem og tilbage mellem spørgsmålene
-    buttons.forEach((button, index) => {
-        if (userAnswers[nuvaerendeStep] === index) {
-            if (index === quizData[nuvaerendeStep].correct) {
-                button.style.backgroundColor = 'green'; // Korrekt svar
-            } else {
-                button.style.backgroundColor = 'red'; // Forkert svar
-            }
-        } else {
-            button.style.backgroundColor = ''; // Reset farven for ikke-valgte knapper
-        }
-    });
+  // Sætter knappernes farve baseret på tidligere valg - så den forbliver når man går frem og tilbage mellem spørgsmålene
+  buttons.forEach((button, index) => {
+    if (userAnswers[nuvaerendeStep] === index) {
+      if (index === quizData[nuvaerendeStep].correct) {
+        button.style.backgroundColor = "green"; // Korrekt svar
+      } else {
+        button.style.backgroundColor = "red"; // Forkert svar
+      }
+    } else {
+      button.style.backgroundColor = ""; // Reset farven for ikke-valgte knapper
+    }
+  });
 }
 
 function loadQuestion() {
-    // Sætter det nuværende spørgsmål
-    spoergsmaalet.innerHTML = quizData[nuvaerendeStep].question;
+  // Sætter det nuværende spørgsmål
+  spoergsmaalet.innerHTML = quizData[nuvaerendeStep].question;
 
-    // Opdaterer knapteksterne med svarmulighederne
-    quizData[nuvaerendeStep].options.forEach((option, index) => {
-        buttons[index].innerHTML = option;
-    });
+  // Opdaterer knapteksterne med svarmulighederne
+  quizData[nuvaerendeStep].options.forEach((option, index) => {
+    buttons[index].innerHTML = option;
+  });
 
-    // Opdaterer knappernes farver (i tilfælde af navigering tilbage med pilene)
-    updateButtonColors();
+  // Opdaterer knappernes farver (i tilfælde af navigering tilbage med pilene)
+  updateButtonColors();
 
-    // Flytter raketten frem når man går et sprøgsmål frem starter ved 248 px og rykker 67 px pr gang, så det passer med stregerne
+    // Flytter raketten frem når man går et sprøgsmål frem starter ved 248 px og rykker 72 px pr gang
     document.getElementById("raketderflyver").style.left = `${248 + (67 * nuvaerendeStep)}px`;
 }
 
 // Tjekker og sender til vinder side, hvis score = 10/10 og loser siden, hvis score er 9/10 eller mindre
 function checkAndDisplayResults() {
-    // Gem scoren i localStorage så vi kan bruge den på loser siden
-    localStorage.setItem("score", score);
+  // Gem scoren i localStorage så vi kan bruge den på loser siden
+  localStorage.setItem("score", score);
 
-    // Gå til vindersiden eller losersiden baseret på scoren
-    if (score === 10) {
-        window.location.href = "vinderside.html";
-    } else {
-        window.location.href = "loserside.html";
-    }
+  // Gå til vindersiden eller losersiden baseret på scoren
+  if (score === 10) {
+    window.location.href = "vinderside.html";
+  } else {
+    window.location.href = "loserside.html";
+  }
 }
 
 // Initialiserer første spørgsmål
 loadQuestion();
 
-
-
-
-
-
 //Array som gemmer forside elementer når slideknap bliver klikket. Bliver gjort ved at tilføje gemforside class som har opacity 0 værdi.
 document.querySelector(".slideknap").addEventListener("click", function () {
-    const gemQuiz = [
-      document.querySelector(".lukknapforside"),
-      document.querySelector("#svarmulighederne_et"),
-      document.querySelector("#svarmulighederne_to"),
-      document.querySelector("#korrekt"),
-      document.querySelector(".forkert"),
-      document.querySelector(".pilknapperne"),
-      document.querySelector("#planeterne"),
-    ];
-  
-    gemQuiz.forEach((element) => {
-      element.classList.toggle("gemquiz");
-    });
+  const gemQuiz = [
+    document.querySelector(".lukknapforside"),
+    document.querySelector("#svarmulighederne_et"),
+    document.querySelector("#svarmulighederne_to"),
+    document.querySelector("#korrekt"),
+    document.querySelector(".forkert"),
+    document.querySelector(".pilknapperne"),
+    document.querySelector("#planeterne"),
+  ];
+
+  gemQuiz.forEach((element) => {
+    element.classList.toggle("gemquiz");
   });
-  
-  //Hvis forside elementer når lukknap bliver klikket
-  document.querySelector(".lukknap").addEventListener("click", function () {
-    const gemQuiz = [
-        document.querySelector(".lukknapforside"),
-        document.querySelector("#svarmulighederne_et"),
-        document.querySelector("#svarmulighederne_to"),
-        document.querySelector("#korrekt"),
-        document.querySelector(".forkert"),
-        document.querySelector(".pilknapperne"),
-        document.querySelector("#planeterne"),
-    ];
-  
-    gemQuiz.forEach((element) => {
-      element.classList.toggle("gemquiz");
-    });
+});
+
+//Hvis forside elementer når lukknap bliver klikket
+document.querySelector(".lukknap").addEventListener("click", function () {
+  const gemQuiz = [
+    document.querySelector(".lukknapforside"),
+    document.querySelector("#svarmulighederne_et"),
+    document.querySelector("#svarmulighederne_to"),
+    document.querySelector("#korrekt"),
+    document.querySelector(".forkert"),
+    document.querySelector(".pilknapperne"),
+    document.querySelector("#planeterne"),
+    document.querySelector(".planetbilleder"),
+  ];
+
+  gemQuiz.forEach((element) => {
+    element.classList.toggle("gemquiz");
   });
-  
-  //Slide in infografik når slideknap bliver klikket. Derudover gem slideknap og vis i stedet lukknap.
-  const slidingImage = document.querySelector(".sliding-billede");
-  const slideKnap = document.querySelector(".slideknap");
-  const lukKnap = document.querySelector(".lukknap");
-  
-  slideKnap.addEventListener("click", function () {
-    slideKnap.style.display = "none";
-    lukKnap.style.display = "block";
-    slidingImage.classList.add("slide-in");
-  });
-  
-  //Slide ud infografik når lukknap bliver trykket. Derudover gem lukknap og vis i stedet slideknap
-  
-  lukKnap.addEventListener("click", function () {
-    lukKnap.style.display = "none";
-    slideKnap.style.display = "block";
-    slidingImage.classList.remove("slide-in");
-  });
-  
+});
+
+//Slide in infografik når slideknap bliver klikket. Derudover gem slideknap og vis i stedet lukknap.
+const slidingImage = document.querySelector(".sliding-billede");
+const slideKnap = document.querySelector(".slideknap");
+const lukKnap = document.querySelector(".lukknap");
+const planeterne = document.querySelector("#planeterne");
+const sprGem = document.querySelector("#spr");
+const fremTilbage = document.querySelector("#pilknapperne");
+
+slideKnap.addEventListener("click", function () {
+  slideKnap.style.display = "none";
+  lukKnap.style.display = "block";
+  planeterne.style.opacity = "0";
+  sprGem.style.opacity = "0";
+  fremTilbage.style.opacity = "0";
+  slidingImage.classList.add("slide-in");
+});
+
+//Slide ud infografik når lukknap bliver trykket. Derudover gem lukknap og vis i stedet slideknap
+
+lukKnap.addEventListener("click", function () {
+  lukKnap.style.display = "none";
+  slideKnap.style.display = "block";
+  planeterne.style.opacity = "1";
+  sprGem.style.opacity = "1";
+  fremTilbage.style.opacity = "1";
+  slidingImage.classList.remove("slide-in");
+});
